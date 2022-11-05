42a79ea4ecb4d0b07c4a8c9f4c2e5d643398220b
JatinSingh08 <singhjatin0812@gmail.com>
2022-11-05 19:38:57 +0530

    updated navbar footer

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import Navbar from '../../Components/Common/Navbar/Navbar'
+import Footer from '../../Components/Common/Footer/Footer'
+import Testimonials from '../../Components/Home/Testimonials/Testimonials'
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar />
+      <Footer />
+      <Testimonials />
+    </div>
   )
 }

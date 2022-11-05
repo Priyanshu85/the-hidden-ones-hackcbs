f74605d136fa6671c2657b03fcf89d1ee685c703
Priyanshu Gupta <pg7909290@gmail.com>
2022-11-06 00:10:37 +0530

    Updates Head

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 // import Cards from '../../Components/Cards/Cards'
 //Components
-// import Head from "../../Components/Home/Head/Head"
+import Head from "../../Components/Home/Head/Head"
 import Navbar from '../../Components/Common/Navbar/Navbar'
 import Footer from '../../Components/Common/Footer/Footer'
-import Testimonials from '../../Components/Home/Testimonials/Testimonials'
-import DoctorsProfile from '../../Components/DoctorsProfile/DoctorsProfile'
+// import Testimonials from '../../Components/Home/Testimonials/Testimonials'
+// import DoctorsProfile from '../../Components/DoctorsProfile/DoctorsProfile'
 import DoctorsCard from '../../Components/Home/DoctorsCard/DoctorsCard'
 // import Hero from '../../Components/Hero/hero'
 export default function Home() {
@@ -13,9 +13,9 @@ export default function Home() {
     <>
       <Navbar />
       {/* <Hero /> */}
-      {/* <Head /> */}
+      <Head />
       <DoctorsCard />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Footer />
     </>
   )

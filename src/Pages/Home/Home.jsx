70ff66ad19d5ecc67fbd1afbde6bf27e1efb61a2
Priyanshu Gupta <pg7909290@gmail.com>
2022-11-06 01:05:47 +0530

    Update Services

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,9 +4,10 @@ import React from 'react'
 import Head from "../../Components/Home/Head/Head"
 import Navbar from '../../Components/Common/Navbar/Navbar'
 import Footer from '../../Components/Common/Footer/Footer'
+import Services from "../../Components/Home/Services/Services"
 // import Testimonials from '../../Components/Home/Testimonials/Testimonials'
 // import DoctorsProfile from '../../Components/DoctorsProfile/DoctorsProfile'
-import DoctorsCard from '../../Components/Home/DoctorsCard/DoctorsCard'
+// import DoctorsCard from '../../Components/Home/DoctorsCard/DoctorsCard'
 // import Hero from '../../Components/Hero/hero'
 export default function Home() {
   return (
@@ -14,7 +15,8 @@ export default function Home() {
       <Navbar />
       {/* <Hero /> */}
       <Head />
-      <DoctorsCard />
+      <Services />
+      {/* <DoctorsCard /> */}
       {/* <Testimonials /> */}
       <Footer />
     </>

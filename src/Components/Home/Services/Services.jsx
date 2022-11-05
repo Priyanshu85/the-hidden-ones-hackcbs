5b0659ee150962cd428fe15e0b4fd428c7c5a61d
AmodSeth <amodseth2002@gmail.com>
2022-11-06 03:21:53 +0530

    Services

--- src/Components/Home/Services/Services.jsx
+++ src/Components/Home/Services/Services.jsx
@@ -1,17 +1,85 @@
-import React from 'react'
+import React from "react";
+import './Services.css';
 
-const Services = () => {
+
+const Services = (props) => {
   return (
     <div>
-  <div className="ServicesSection">
-    <div className="ServicesContent">
-      <h2 className="ServicesTitle">ROOF PARTY POLAROID</h2>
-      <h1 className="ServicesHeading">Master Cleanse Reliac Heirloom</h1>
-      <p className="ServicesText">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+      <div className="ServicesSection">
+        <div className="ServicesContent">
+          <h2 className="ServicesTitle tracking-in-expand">Services Offered By us</h2>
+          <h4 className="ServicesHeading focus-in-expand-fwd ">Consult top doctors online for any health concern</h4>
+          <p className="ServicesText focus-in-expand-fwd ">
+          Book an appointment for an in-clinic consultation
+Find experienced doctors across all specialties
+          </p>
+        </div>
+      </div>
+      {/* //card div */}
+    <div className="card_container">
+      <div className="card-category-1">
+            <div className="basic-card basic-card-aqua">
+                <div className="card-content">
+            <span className="card-title">Best-offer</span>
+                    <p className="card-text">
+              Get the Heaviest Discounts ever..
+              From solo offers to Family package all in one.. 
+                  </p>
+                </div>
+
+                <div className="card-link">
+                    <a title="Read Full"href="#_"><span>Read Full</span></a>
+            </div>
+            </div>
+      <div className="card-category-1">
+            <div className="basic-card basic-card-aqua">
+                <div className="card-content">
+            <span className="card-title">Find Doctors Near You</span>
+                    <p className="card-text">
+                  Get the Best Doctors that you want..From Establishing trust
+                  to building loyalty
+                  </p>
+                </div>
+
+                <div className="card-link">
+                    <a title="Read Full" href="#_"><span>Read Full</span></a>
+            </div>
+            </div>
+      <div className="card-category-1">
+            <div className="basic-card basic-card-aqua">
+                <div className="card-content">
+            <span className="card-title">Hassle Free life</span>
+                    <p className="card-text">
+                    In the time of suffering, have a stress free recovery 
+                    rather than roaming in hospitals.
+                  </p>
+                </div>
+
+                <div className="card-link">
+                    <a title="Read Full" href="#_" ><span>Read Full</span></a>
+                </div>
+                </div>
+              </div>
+      <div className="card-category-1">
+            <div className="basic-card basic-card-aqua">
+                <div className="card-content">
+            <span className="card-title">Special Programs</span>
+                    <p className="card-text">
+                    Special program that supports your life style
+                    through various Diseases.
+                  </p>
+                </div>
+
+                <div className="card-link">
+                    <a title="Read Full" href="#_" ><span>Read Full</span></a>
+                </div>
+                </div>
+              </div>
+              </div>
+        </div>
+        </div>
     </div>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Services;
